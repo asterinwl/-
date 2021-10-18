226edfc12d9d289326e9e5572f599799e7008eb9
asterinwl <85321878+asterinwl@users.noreply.github.com>
2021-10-19 01:00:21 +0900

    Delete 6058_21961350(AC).py

--- 6058_21961350(AC).py
+++
@@ -1,8 +0,0 @@
-﻿# 2개의 정수값이 입력될 때,
-# 그 불 값(True/False) 이 모두 False 일 때에만 True 를 출력하는 프로그램을 작성해보자.
-
-a,b=input().split()
-print((not bool(int(a))) and (not bool(int(b))))
-
-# 정답
-# 한꺼번에 써야지 오류가 발생하지 않는다.
