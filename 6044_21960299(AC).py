a932cc3aad84616a2635a5085d7d0968eda2daa7
asterinwl <85321878+asterinwl@users.noreply.github.com>
2021-10-18 23:50:14 +0900

    Add files via upload

---
+++ 6044_21960299(AC).py
@@ -0,0 +1,23 @@
+﻿# 첫 번째 줄에 합
+# 두 번째 줄에 차,
+# 세 번째 줄에 곱,
+# 네 번째 줄에 몫,
+# 다섯 번째 줄에 나머지,
+# 여섯 번째 줄에 나눈 값을 순서대로 출력한다.
+# (실수, 소수점 이하 둘째 자리까지의 정확도로 출력)
+
+a,b=input().split()
+
+c=int(a)+int(b)
+d=int(a)-int(b)
+e=int(a)*int(b)
+f=int(a)//int(b)
+g=int(a)%int(b)
+h=int(a)/int(b)
+
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(format(h,".2f"))
---
+++ 6058_21961291(WA).py
@@ -0,0 +1,10 @@
+﻿# 2개의 정수값이 입력될 때,
+# 그 불 값(True/False) 이 모두 False 일 때에만 True 를 출력하는 프로그램을 작성해보자.
+
+a,b=input().split()
+c=bool(int(a))
+d=bool(int(b))
+print(((not c) and (not d))
+
+# 오류 발생
+# c와 d로 나누는 경우는 오류가 생긴다.
